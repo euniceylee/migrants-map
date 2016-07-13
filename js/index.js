30afd26aaa093e21869403f11e86934b4acebb0b
euniceylee <euniceyl@u.northwestern.edu>
2016-07-13 16:18:28 -0400

    cities are up (but not labeled), and water is blue

--- js/index.js
+++ js/index.js
@@ -2,8 +2,8 @@ var width = 960,
     height = 500;
 
 var projection = d3.geo.mercator()
-    .translate([480, 320])
-    .scale(300);
+    .translate([480, 340])
+    .scale(308);
 
 var svg = d3.select("body").append("svg")
     .attr("width", width)
@@ -18,25 +18,21 @@ var g = svg.append("g");
 d3.json("world-topo-min.json", function(error, topology) {
 
 // load and display the cities
-d3.csv("final-refugee-athletes-1.csv", function(error, data) {
-    g.selectAll("circle")
+d3.csv("final-refugee-athletes-2.csv", function(data) {
+    svg.selectAll("circle")
        .data(data)
        .enter()
-       .append("a")
-                  .attr("xlink:href", function(d) {
-                      return "https://www.google.com/search?q="+d.city;}
-                  )
-       .append("circle")
+       .append("circle") //idk what this does
        .attr("cx", function(d) {
                return projection([d.lon, d.lat])[0];
        })
        .attr("cy", function(d) {
                return projection([d.lon, d.lat])[1];
        })
-       .attr("r", 5)
-       .style("fill", "red");
+       .attr("r", 3)
+       .style("fill", "yellow")
+       .style("stroke", "black");
 });
-
 
 g.selectAll("path")
       .data(topojson.object(topology, topology.objects.countries)
@@ -47,16 +43,15 @@ g.selectAll("path")
 });
 
 // zoom and pan
-/*var zoom = d3.behavior.zoom()
-    .on("zoom",function() {
-        g.attr("transform","translate("+ 
-            d3.event.translate.join(",")+")scale("+d3.event.scale+")");
-        g.selectAll("circle")
-            .attr("d", path.projection(projection));
-        g.selectAll("path")  
-            .attr("d", path.projection(projection)); 
+// var zoom = d3.behavior.zoom()
+//     .on("zoom",function() {
+//         g.attr("transform","translate("+ 
+//             d3.event.translate.join(",")+")scale("+d3.event.scale+")");
+//         g.selectAll("circle")
+//             .attr("d", path.projection(projection));
+//         g.selectAll("path")  
+//             .attr("d", path.projection(projection)); 
 
-  });
+//   });
 
-svg.call(zoom)
-*/
+// svg.call(zoom)
